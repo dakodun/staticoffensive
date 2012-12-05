29e6ba25cbcfc25642ae9caca1bdcf20d31b2528
dakodun <jinto@chikin.net>
2012-12-05 11:56:55 +0000

    loading new resources

--- parts/initscene.js
+++ parts/initscene.js
@@ -16,7 +16,19 @@ InitScene.prototype.Persistent = function() {
 
 // initialises the scene object
 InitScene.prototype.SetUp = function() {
-	
+	try {
+		nmgrs.resLoad.QueueTexture("tileset_test", "./res/vis/tileset_test.png");
+		
+		nmgrs.resLoad.QueueTexture("gui_map_compassmain", "./res/vis/gui_map_compassmain.png");
+		nmgrs.resLoad.QueueTexture("gui_map_compassextra", "./res/vis/gui_map_compassextra.png");
+		nmgrs.resLoad.QueueTexture("gui_map_zlevelmain", "./res/vis/gui_map_zlevelmain.png");
+		nmgrs.resLoad.QueueTexture("gui_map_zlevelextra", "./res/vis/gui_map_zlevelextra.png");
+		
+		nmgrs.resLoad.AcquireResources();
+		nmgrs.resLoad.mIntervalID = setInterval(function() {nmgrs.resLoad.ProgressCheck();}, 0);
+	} catch(e) {
+		alert(e.What());
+	}
 }
 
 // cleans up the scene object
@@ -31,7 +43,9 @@ InitScene.prototype.Input = function() {
 
 // handles game logic
 InitScene.prototype.Process = function() {
-	
+	if (nmgrs.resLoad.mWorking == false) {
+		nmgrs.sceneMan.ChangeScene(new GFTestScene());
+	}
 }
 
 // handles all drawing tasks
