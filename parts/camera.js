3687ee274038bd8a4cd1d61d3fdb0b79c851c7d5
dakodun <jinto@chikin.net>
2012-12-05 11:55:47 +0000

    added translate function; add indication of whether or not the view (camera state) has changed

--- parts/camera.js
+++ parts/camera.js
@@ -3,6 +3,8 @@
 // todo: maintain a transform matrix for translation as well as rotation and scaling
 function Camera() {
 	this.mTranslate = new IVec2(0, 0); // current translation
+	
+	this.mViewUpdated = false; // indication whether the camera state has been altered
 }
 
 // make a copy of another (other) camera (copy constructor)
@@ -10,9 +12,22 @@ Camera.prototype.Copy = function(other) {
 	this.mTranslate.Copy(other.mTranslate); // call ivec2 copy (copy constructor)
 }
 
+// processes camera every frame
+Camera.prototype.Process = function() {
+	if (this.mViewUpdated == true) {
+		this.mViewUpdated = false;
+	}
+}
+
 // apply the camera's transform to the canvas
 Camera.prototype.Apply = function() {
 	nmain.game.mCurrContext.translate(-this.mTranslate.mX, -this.mTranslate.mY); // apply translation
+}
+
+// apply a transformation to the camera
+Camera.prototype.Translate = function(trans) {
+	this.mTranslate.Set(this.mTranslate.mX - trans.mX, this.mTranslate.mY - trans.mY);
+	this.mViewUpdated = true;
 }
 // ...End
 
