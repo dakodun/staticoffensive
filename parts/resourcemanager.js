c8a57981907e847fa9ccecfa2bfa746099a756fb
dakodun <jinto@chikin.net>
2012-12-07 13:39:51 +0000

    added new font store

--- parts/resourcemanager.js
+++ parts/resourcemanager.js
@@ -2,6 +2,7 @@
 // holds the resource stores for each individual resource type
 function ResourceManager() {
 	this.mTexStore = new ResourceStore(); // storage for our textures
+	this.mFontStore = new ResourceStore(); // storage for our fonts
 };
 // ...End
 
