baf3a59216ddb436fec969ed6fb1144ecb94a310
dakodun <jinto@chikin.net>
2012-12-14 15:05:21 +0000

    added a 'new' dialogue; added a 'blackout' for when a dialogue is open; added a switch that indicates whether we have an open dialogue or not

--- parts/gamefiles/creation/gfguicreationcontrol.js
+++ parts/gamefiles/creation/gfguicreationcontrol.js
@@ -5,6 +5,10 @@ function GFGUICreationControl() {
 	
 	this.mTopBar = new GFGUICreationBar();
 	this.mTileControl = new GFGUICreationTileControl();
+	this.mNewDialogue = new GFGUICreationNewDialogue();
+	
+	this.mDialogueOpen = false;
+	this.mBlackout = new Shape();
 }
 
 GFGUICreationControl.prototype.SetUp = function(initTex) {
@@ -15,13 +19,30 @@ GFGUICreationControl.prototype.SetUp = function(initTex) {
 	
 	this.mTopBar.SetUp(initOffset);
 	this.mTileControl.SetUp(initTex, initOffset);
+	this.mNewDialogue.SetUp(initOffset);
+	
+	{
+		this.mBlackout.mPos.Copy(initOffset);
+		this.mBlackout.mDepth = -5005;
+		this.mBlackout.mColour = "#000000";
+		this.mBlackout.mAlpha = 0.75;
+		
+		this.mBlackout.AddPoint(new IVec2(nmain.game.mCanvasSize.mX, 0));
+		this.mBlackout.AddPoint(new IVec2(nmain.game.mCanvasSize.mX, nmain.game.mCanvasSize.mY));
+		this.mBlackout.AddPoint(new IVec2(0, nmain.game.mCanvasSize.mY));
+	}
 	
 	this.mTranslate.Copy(currScene.mCam.mTranslate);
 };
 
 GFGUICreationControl.prototype.Input = function() {
-	this.mTopBar.Input();
-	this.mTileControl.Input();
+	if (this.mDialogueOpen == false) {
+		this.mTopBar.Input();
+		this.mTileControl.Input();
+	}
+	else {
+		this.mNewDialogue.Input();
+	}
 }
 
 GFGUICreationControl.prototype.Process = function() {
@@ -34,8 +55,22 @@ GFGUICreationControl.prototype.Process = function() {
 		var pt = new IVec2(0, 0);
 		pt.Copy(nmgrs.inputMan.GetLocalMouseCoords());
 		
-		this.mTopBar.Process(pt, offset);
-		this.mTileControl.Process(pt, offset);
+		if (this.mDialogueOpen == false) {
+			this.mTopBar.Process(pt);
+			this.mTileControl.Process(pt);
+		}
+		else {
+			this.mNewDialogue.Process(pt);
+		}
+	}
+	
+	{
+		this.mTopBar.UpdatePosition(offset);
+		this.mTileControl.UpdatePosition(offset);
+		this.mNewDialogue.UpdatePosition(offset);
+		
+		this.mBlackout.mPos.mX += offset.mX;
+		this.mBlackout.mPos.mY += offset.mY;
 	}
 	
 	this.mTranslate.Copy(currScene.mCam.mTranslate);
@@ -46,6 +81,11 @@ GFGUICreationControl.prototype.GetRenderData = function() {
 	
 	arr = arr.concat(this.mTopBar.GetRenderData());
 	arr = arr.concat(this.mTileControl.GetRenderData());
+	
+	if (this.mDialogueOpen == true) {
+		arr.push(this.mBlackout);
+		arr = arr.concat(this.mNewDialogue.GetRenderData());
+	}
 	
 	return arr;
 }
