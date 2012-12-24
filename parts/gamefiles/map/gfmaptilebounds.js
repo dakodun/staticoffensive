9b628250c1d226b8358a3c3f6530d4d2bb525222
dakodun <jinto@chikin.net>
2012-12-24 12:18:10 +0000

    update bounds to better fit the higher res textures; changed slope bounds to be a near fit as an exact fit would result in a concave polygon

--- parts/gamefiles/map/gfmaptilebounds.js
+++ parts/gamefiles/map/gfmaptilebounds.js
@@ -5,95 +5,72 @@ function GFMapTileBounds() {
 	
 	{
 		this.mBounds[0] = new Shape();
-		this.mBounds[0].mPos.Set(			0,  38);
-		this.mBounds[0].AddPoint(new IVec2(28, -14));
-		this.mBounds[0].AddPoint(new IVec2(31, -14));
-		this.mBounds[0].AddPoint(new IVec2(59,   0));
-		this.mBounds[0].AddPoint(new IVec2(59,   7));
-		this.mBounds[0].AddPoint(new IVec2(31,  21));
-		this.mBounds[0].AddPoint(new IVec2(28,  21));
-		this.mBounds[0].AddPoint(new IVec2( 0,  7));
+		this.mBounds[0].mPos.Set(			0,  39);
+		this.mBounds[0].AddPoint(new IVec2(31, -15));
+		this.mBounds[0].AddPoint(new IVec2(61,   0));
+		this.mBounds[0].AddPoint(new IVec2(61,   6));
+		this.mBounds[0].AddPoint(new IVec2(30,  21));
+		this.mBounds[0].AddPoint(new IVec2( 0,   6));
 	}
 	
 	{
 		this.mBounds[1] = new Shape();
-		this.mBounds[1].mPos.Set(			0,  30);
-		this.mBounds[1].AddPoint(new IVec2(28, -14));
-		this.mBounds[1].AddPoint(new IVec2(31, -14));
-		this.mBounds[1].AddPoint(new IVec2(35, -12));
-		this.mBounds[1].AddPoint(new IVec2(53,   6));
-		this.mBounds[1].AddPoint(new IVec2(55,   6));
-		this.mBounds[1].AddPoint(new IVec2(59,   8));
-		this.mBounds[1].AddPoint(new IVec2(59,  15));
-		this.mBounds[1].AddPoint(new IVec2(31,  29));
-		this.mBounds[1].AddPoint(new IVec2(28,  29));
-		this.mBounds[1].AddPoint(new IVec2( 0,  15));
+		this.mBounds[1].mPos.Set(			0,  31);
+		this.mBounds[1].AddPoint(new IVec2(31, -15));
+		this.mBounds[1].AddPoint(new IVec2(61,   8));
+		this.mBounds[1].AddPoint(new IVec2(61,  14));
+		this.mBounds[1].AddPoint(new IVec2(30,  29));
+		this.mBounds[1].AddPoint(new IVec2( 0,  14));
 	}
-	
+		
 	{
 		this.mBounds[2] = new Shape();
-		this.mBounds[2].mPos.Set(			0,  30);
-		this.mBounds[2].AddPoint(new IVec2(28, -14));
-		this.mBounds[2].AddPoint(new IVec2(31, -14));
-		this.mBounds[2].AddPoint(new IVec2(59,   0));
-		this.mBounds[2].AddPoint(new IVec2(59,  15));
-		this.mBounds[2].AddPoint(new IVec2(31,  29));
-		this.mBounds[2].AddPoint(new IVec2(28,  29));
-		this.mBounds[2].AddPoint(new IVec2( 0,  15));
+		this.mBounds[2].mPos.Set(			0,  31);
+		this.mBounds[2].AddPoint(new IVec2(31, -15));
+		this.mBounds[2].AddPoint(new IVec2(61,   0));
+		this.mBounds[2].AddPoint(new IVec2(61,  14));
+		this.mBounds[2].AddPoint(new IVec2(30,  29));
+		this.mBounds[2].AddPoint(new IVec2( 0,  14));
 	}
 	
 	{
 		this.mBounds[3] = new Shape();
-		this.mBounds[3].mPos.Set(			0,  22);
-		this.mBounds[3].AddPoint(new IVec2(28, -14));
-		this.mBounds[3].AddPoint(new IVec2(31, -14));
-		this.mBounds[3].AddPoint(new IVec2(35, -12));
-		this.mBounds[3].AddPoint(new IVec2(53,   6));
-		this.mBounds[3].AddPoint(new IVec2(55,   6));
-		this.mBounds[3].AddPoint(new IVec2(59,   8));
-		this.mBounds[3].AddPoint(new IVec2(59,  23));
-		this.mBounds[3].AddPoint(new IVec2(31,  37));
-		this.mBounds[3].AddPoint(new IVec2(28,  37));
-		this.mBounds[3].AddPoint(new IVec2( 0,  23));
+		this.mBounds[3].mPos.Set(			0,  23);
+		this.mBounds[3].AddPoint(new IVec2(31, -15));
+		this.mBounds[3].AddPoint(new IVec2(61,  8));
+		this.mBounds[3].AddPoint(new IVec2(61,  22));
+		this.mBounds[3].AddPoint(new IVec2(30,  37));
+		this.mBounds[3].AddPoint(new IVec2( 0,  22));
 	}
 	
 	{
 		this.mBounds[4] = new Shape();
-		this.mBounds[4].mPos.Set(			0,  22);
-		this.mBounds[4].AddPoint(new IVec2(28, -14));
-		this.mBounds[4].AddPoint(new IVec2(31, -14));
-		this.mBounds[4].AddPoint(new IVec2(59,   0));
-		this.mBounds[4].AddPoint(new IVec2(59,  23));
-		this.mBounds[4].AddPoint(new IVec2(31,  37));
-		this.mBounds[4].AddPoint(new IVec2(28,  37));
-		this.mBounds[4].AddPoint(new IVec2( 0,  23));
+		this.mBounds[4].mPos.Set(			0,  23);
+		this.mBounds[4].AddPoint(new IVec2(31, -15));
+		this.mBounds[4].AddPoint(new IVec2(61,   0));
+		this.mBounds[4].AddPoint(new IVec2(61,  22));
+		this.mBounds[4].AddPoint(new IVec2(30,  37));
+		this.mBounds[4].AddPoint(new IVec2( 0,  22));
 	}
 	
 	{
 		this.mBounds[5] = new Shape();
-		this.mBounds[5].mPos.Set(			0,  14);
-		this.mBounds[5].AddPoint(new IVec2(28, -14));
-		this.mBounds[5].AddPoint(new IVec2(31, -14));
-		this.mBounds[5].AddPoint(new IVec2(35, -12));
-		this.mBounds[5].AddPoint(new IVec2(53,   6));
-		this.mBounds[5].AddPoint(new IVec2(55,   6));
-		this.mBounds[5].AddPoint(new IVec2(59,   8));
-		this.mBounds[5].AddPoint(new IVec2(59,  31));
-		this.mBounds[5].AddPoint(new IVec2(31,  45));
-		this.mBounds[5].AddPoint(new IVec2(28,  45));
-		this.mBounds[5].AddPoint(new IVec2( 0,  31));
+		this.mBounds[5].mPos.Set(			0,  15);
+		this.mBounds[5].AddPoint(new IVec2(31, -15));
+		this.mBounds[5].AddPoint(new IVec2(61,  8));
+		this.mBounds[5].AddPoint(new IVec2(61,  30));
+		this.mBounds[5].AddPoint(new IVec2(30,  45));
+		this.mBounds[5].AddPoint(new IVec2( 0,  30));
 	}
 	
 	{
 		this.mBounds[6] = new Shape();
-		this.mBounds[6].mPos.Set(			0,  14);
-		this.mBounds[6].AddPoint(new IVec2(28, -14));
-		this.mBounds[6].AddPoint(new IVec2(31, -14));
-		this.mBounds[6].AddPoint(new IVec2(59,   0));
-		this.mBounds[6].AddPoint(new IVec2(59,  31));
-		this.mBounds[6].AddPoint(new IVec2(31,  45));
-		this.mBounds[6].AddPoint(new IVec2(28,  45));
-		this.mBounds[6].AddPoint(new IVec2( 0,  31));
+		this.mBounds[6].mPos.Set(			0,  15);
+		this.mBounds[6].AddPoint(new IVec2(31, -15));
+		this.mBounds[6].AddPoint(new IVec2(61,   0));
+		this.mBounds[6].AddPoint(new IVec2(61,  30));
+		this.mBounds[6].AddPoint(new IVec2(30,  45));
+		this.mBounds[6].AddPoint(new IVec2( 0,  30));
 	}
 	
 	{
@@ -103,165 +80,122 @@ function GFMapTileBounds() {
 	
 	{
 		this.mBounds[8] = new Shape();
-		this.mBounds[8].mPos.Set(			0,  38);
-		this.mBounds[8].AddPoint(new IVec2( 6,  -2));
-		this.mBounds[8].AddPoint(new IVec2(24, -20));
-		this.mBounds[8].AddPoint(new IVec2(28, -22));
-		this.mBounds[8].AddPoint(new IVec2(31, -22));
-		this.mBounds[8].AddPoint(new IVec2(59,  -8));
-		this.mBounds[8].AddPoint(new IVec2(59,   7));
-		this.mBounds[8].AddPoint(new IVec2(31,  21));
-		this.mBounds[8].AddPoint(new IVec2(28,  21));
-		this.mBounds[8].AddPoint(new IVec2( 0,   7));
+		this.mBounds[8].mPos.Set(			0,  39);
+		this.mBounds[8].AddPoint(new IVec2(31, -23));
+		this.mBounds[8].AddPoint(new IVec2(61,  -8));
+		this.mBounds[8].AddPoint(new IVec2(61,   6));
+		this.mBounds[8].AddPoint(new IVec2(30,  21));
+		this.mBounds[8].AddPoint(new IVec2( 0,   6));
 	}
 	
 	{
 		this.mBounds[10] = new Shape();
-		this.mBounds[10].mPos.Set(			 0,  30);
-		this.mBounds[10].AddPoint(new IVec2( 6,  -2));
-		this.mBounds[10].AddPoint(new IVec2(24, -20));
-		this.mBounds[10].AddPoint(new IVec2(28, -22));
-		this.mBounds[10].AddPoint(new IVec2(31, -22));
-		this.mBounds[10].AddPoint(new IVec2(59,  -8));
-		this.mBounds[10].AddPoint(new IVec2(59,  15));
-		this.mBounds[10].AddPoint(new IVec2(31,  29));
-		this.mBounds[10].AddPoint(new IVec2(28,  29));
-		this.mBounds[10].AddPoint(new IVec2( 0,  15));
+		this.mBounds[10].mPos.Set(			0,   31);
+		this.mBounds[10].AddPoint(new IVec2(31, -23));
+		this.mBounds[10].AddPoint(new IVec2(61,  -8));
+		this.mBounds[10].AddPoint(new IVec2(61,  14));
+		this.mBounds[10].AddPoint(new IVec2(30,  29));
+		this.mBounds[10].AddPoint(new IVec2( 0,  14));
 	}
 	
 	{
 		this.mBounds[12] = new Shape();
-		this.mBounds[12].mPos.Set(			 0,  22);
-		this.mBounds[12].AddPoint(new IVec2( 6,  -2));
-		this.mBounds[12].AddPoint(new IVec2(24, -20));
-		this.mBounds[12].AddPoint(new IVec2(28, -22));
-		this.mBounds[12].AddPoint(new IVec2(31, -22));
-		this.mBounds[12].AddPoint(new IVec2(59,  -8));
-		this.mBounds[12].AddPoint(new IVec2(59,  23));
-		this.mBounds[12].AddPoint(new IVec2(31,  37));
-		this.mBounds[12].AddPoint(new IVec2(28,  37));
-		this.mBounds[12].AddPoint(new IVec2( 0,  23));
+		this.mBounds[12].mPos.Set(			0,   23);
+		this.mBounds[12].AddPoint(new IVec2(31, -23));
+		this.mBounds[12].AddPoint(new IVec2(61,  -8));
+		this.mBounds[12].AddPoint(new IVec2(61,  22));
+		this.mBounds[12].AddPoint(new IVec2(30,  37));
+		this.mBounds[12].AddPoint(new IVec2( 0,  22));
 	}
 	
 	{
 		this.mBounds[15] = new Shape();
-		this.mBounds[15].mPos.Set(			 0,  38);
-		this.mBounds[15].AddPoint(new IVec2(28, -14));
-		this.mBounds[15].AddPoint(new IVec2(31, -14));
-		this.mBounds[15].AddPoint(new IVec2(39, -10));
-		this.mBounds[15].AddPoint(new IVec2(55, -10));
-		this.mBounds[15].AddPoint(new IVec2(59,  -8));
-		this.mBounds[15].AddPoint(new IVec2(59,   7));
-		this.mBounds[15].AddPoint(new IVec2(31,  21));
-		this.mBounds[15].AddPoint(new IVec2(28,  21));
-		this.mBounds[15].AddPoint(new IVec2( 0,   7));
+		this.mBounds[15].mPos.Set(			0,  39);
+		this.mBounds[15].AddPoint(new IVec2(31, -15));
+		this.mBounds[15].AddPoint(new IVec2(61,  -8));
+		this.mBounds[15].AddPoint(new IVec2(61,   6));
+		this.mBounds[15].AddPoint(new IVec2(30,  21));
+		this.mBounds[15].AddPoint(new IVec2( 0,   6));
 	}
 	
 	{
 		this.mBounds[17] = new Shape();
-		this.mBounds[17].mPos.Set(			 0,  30);
-		this.mBounds[17].AddPoint(new IVec2(28, -14));
-		this.mBounds[17].AddPoint(new IVec2(31, -14));
-		this.mBounds[17].AddPoint(new IVec2(39, -10));
-		this.mBounds[17].AddPoint(new IVec2(55, -10));
-		this.mBounds[17].AddPoint(new IVec2(59,  -8));
-		this.mBounds[17].AddPoint(new IVec2(59,  15));
-		this.mBounds[17].AddPoint(new IVec2(31,  29));
-		this.mBounds[17].AddPoint(new IVec2(28,  29));
-		this.mBounds[17].AddPoint(new IVec2( 0,  15));
+		this.mBounds[17].mPos.Set(			0,  31);
+		this.mBounds[17].AddPoint(new IVec2(31, -15));
+		this.mBounds[17].AddPoint(new IVec2(61,  -8));
+		this.mBounds[17].AddPoint(new IVec2(61,  14));
+		this.mBounds[17].AddPoint(new IVec2(30,  29));
+		this.mBounds[17].AddPoint(new IVec2( 0,  14));
 	}
 	
 	{
 		this.mBounds[19] = new Shape();
-		this.mBounds[19].mPos.Set(			 0,  22);
-		this.mBounds[19].AddPoint(new IVec2(28, -14));
-		this.mBounds[19].AddPoint(new IVec2(31, -14));
-		this.mBounds[19].AddPoint(new IVec2(39, -10));
-		this.mBounds[19].AddPoint(new IVec2(55, -10));
-		this.mBounds[19].AddPoint(new IVec2(59,  -8));
-		this.mBounds[19].AddPoint(new IVec2(59,  23));
-		this.mBounds[19].AddPoint(new IVec2(31,  37));
-		this.mBounds[19].AddPoint(new IVec2(28,  37));
-		this.mBounds[19].AddPoint(new IVec2( 0,  23));
+		this.mBounds[19].mPos.Set(			0,  23);
+		this.mBounds[19].AddPoint(new IVec2(31, -15));
+		this.mBounds[19].AddPoint(new IVec2(61,  -8));
+		this.mBounds[19].AddPoint(new IVec2(61,  22));
+		this.mBounds[19].AddPoint(new IVec2(30,  37));
+		this.mBounds[19].AddPoint(new IVec2( 0,  22));
 	}
 	
 	{
 		this.mBounds[22] = new Shape();
-		this.mBounds[22].mPos.Set(			 0,  30);
-		this.mBounds[22].AddPoint(new IVec2( 4,  -2));
-		this.mBounds[22].AddPoint(new IVec2(20,  -2));
-		this.mBounds[22].AddPoint(new IVec2(28,  -6));
-		this.mBounds[22].AddPoint(new IVec2(31,  -6));
-		this.mBounds[22].AddPoint(new IVec2(59,   8));
-		this.mBounds[22].AddPoint(new IVec2(59,  15));
-		this.mBounds[22].AddPoint(new IVec2(31,  29));
-		this.mBounds[22].AddPoint(new IVec2(28,  29));
-		this.mBounds[22].AddPoint(new IVec2( 0,  15));
+		this.mBounds[22].mPos.Set(			0,  31);
+		this.mBounds[22].AddPoint(new IVec2(31,  -7));
+		this.mBounds[22].AddPoint(new IVec2(61,   8));
+		this.mBounds[22].AddPoint(new IVec2(61,  14));
+		this.mBounds[22].AddPoint(new IVec2(30,  29));
+		this.mBounds[22].AddPoint(new IVec2( 0,  14));
 	}
 	
 	{
 		this.mBounds[24] = new Shape();
-		this.mBounds[24].mPos.Set(			 0,  22);
-		this.mBounds[24].AddPoint(new IVec2( 4,  -2));
-		this.mBounds[24].AddPoint(new IVec2(20,  -2));
-		this.mBounds[24].AddPoint(new IVec2(28,  -6));
-		this.mBounds[24].AddPoint(new IVec2(31,  -6));
-		this.mBounds[24].AddPoint(new IVec2(59,   8));
-		this.mBounds[24].AddPoint(new IVec2(59,  23));
-		this.mBounds[24].AddPoint(new IVec2(31,  37));
-		this.mBounds[24].AddPoint(new IVec2(28,  37));
-		this.mBounds[24].AddPoint(new IVec2( 0,  23));
+		this.mBounds[24].mPos.Set(			0,  23);
+		this.mBounds[24].AddPoint(new IVec2(31,  -7));
+		this.mBounds[24].AddPoint(new IVec2(61,   8));
+		this.mBounds[24].AddPoint(new IVec2(61,  22));
+		this.mBounds[24].AddPoint(new IVec2(30,  37));
+		this.mBounds[24].AddPoint(new IVec2( 0,  22));
 	}
 	
 	{
 		this.mBounds[26] = new Shape();
-		this.mBounds[26] = new Shape();
-		this.mBounds[26].mPos.Set(			 0,  14);
-		this.mBounds[26].AddPoint(new IVec2( 4,  -2));
-		this.mBounds[26].AddPoint(new IVec2(20,  -2));
-		this.mBounds[26].AddPoint(new IVec2(28,  -6));
-		this.mBounds[26].AddPoint(new IVec2(31,  -6));
-		this.mBounds[26].AddPoint(new IVec2(59,   8));
-		this.mBounds[26].AddPoint(new IVec2(59,  31));
-		this.mBounds[26].AddPoint(new IVec2(31,  45));
-		this.mBounds[26].AddPoint(new IVec2(28,  45));
-		this.mBounds[26].AddPoint(new IVec2( 0,  31));
+		this.mBounds[26].mPos.Set(			0,  15);
+		this.mBounds[26].AddPoint(new IVec2(31,  -7));
+		this.mBounds[26].AddPoint(new IVec2(61,   8));
+		this.mBounds[26].AddPoint(new IVec2(61,  30));
+		this.mBounds[26].AddPoint(new IVec2(30,  45));
+		this.mBounds[26].AddPoint(new IVec2( 0,  30));
 	}
 	
 	{
 		this.mBounds[32] = new Shape();
-		this.mBounds[32].mPos.Set(			0,  22);
-		this.mBounds[32].AddPoint(new IVec2(28, -14));
-		this.mBounds[32].AddPoint(new IVec2(31, -14));
-		this.mBounds[32].AddPoint(new IVec2(59,   0));
-		this.mBounds[32].AddPoint(new IVec2(59,  23));
-		this.mBounds[32].AddPoint(new IVec2(31,  37));
-		this.mBounds[32].AddPoint(new IVec2(28,  37));
-		this.mBounds[32].AddPoint(new IVec2( 0,  23));
+		this.mBounds[32].mPos.Set(			0,  23);
+		this.mBounds[32].AddPoint(new IVec2(31, -15));
+		this.mBounds[32].AddPoint(new IVec2(61,   0));
+		this.mBounds[32].AddPoint(new IVec2(61,  22));
+		this.mBounds[32].AddPoint(new IVec2(30,  37));
+		this.mBounds[32].AddPoint(new IVec2( 0,  22));
 	}
 	
 	{
 		this.mBounds[33] = new Shape();
-		this.mBounds[33].mPos.Set(			0,  30);
-		this.mBounds[33].AddPoint(new IVec2(28, -14));
-		this.mBounds[33].AddPoint(new IVec2(31, -14));
-		this.mBounds[33].AddPoint(new IVec2(59,   0));
-		this.mBounds[33].AddPoint(new IVec2(59,  15));
-		this.mBounds[33].AddPoint(new IVec2(31,  29));
-		this.mBounds[33].AddPoint(new IVec2(28,  29));
-		this.mBounds[33].AddPoint(new IVec2( 0,  15));
+		this.mBounds[33].mPos.Set(			0,  31);
+		this.mBounds[33].AddPoint(new IVec2(31, -15));
+		this.mBounds[33].AddPoint(new IVec2(61,   0));
+		this.mBounds[33].AddPoint(new IVec2(61,  14));
+		this.mBounds[33].AddPoint(new IVec2(30,  29));
+		this.mBounds[33].AddPoint(new IVec2( 0,  14));
 	}
 	
 	{
 		this.mBounds[34] = new Shape();
-		this.mBounds[34].mPos.Set(			0,  38);
-		this.mBounds[34].AddPoint(new IVec2(28, -14));
-		this.mBounds[34].AddPoint(new IVec2(31, -14));
-		this.mBounds[34].AddPoint(new IVec2(59,   0));
-		this.mBounds[34].AddPoint(new IVec2(59,   7));
-		this.mBounds[34].AddPoint(new IVec2(31,  21));
-		this.mBounds[34].AddPoint(new IVec2(28,  21));
-		this.mBounds[34].AddPoint(new IVec2( 0,  7));
+		this.mBounds[34].mPos.Set(			0,  39);
+		this.mBounds[34].AddPoint(new IVec2(31, -15));
+		this.mBounds[34].AddPoint(new IVec2(61,   0));
+		this.mBounds[34].AddPoint(new IVec2(61,   6));
+		this.mBounds[34].AddPoint(new IVec2(30,  21));
+		this.mBounds[34].AddPoint(new IVec2( 0,   6));
 	}
 };
 // ...End
