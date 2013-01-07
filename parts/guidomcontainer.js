af4b50b36e3c55130a33ccebb7cb456e9698dd3e
dakodun <jinto@chikin.net>
2013-01-07 14:53:05 +0000

    call gui element's 'process' method; renamed 'add' and 'remove' methods for consistency; added 'getelement' method to return a gui dom element; register and unregister an element's callbacks when adding and removing

--- parts/guidomcontainer.js
+++ parts/guidomcontainer.js
@@ -15,20 +15,22 @@ function GUIDOMContainer() {
 };
 
 GUIDOMContainer.prototype.Process = function() {
-	if (this.mOldCanvas.mX != nmain.game.mCanvasPos.mX || this.mOldCanvas.mY != nmain.game.mCanvasPos.mY) {
-		var diff = new IVec2(nmain.game.mCanvasPos.mX - this.mOldCanvas.mX,
-				nmain.game.mCanvasPos.mY - this.mOldCanvas.mY);
+	for (var i = 0; i < this.mElements.length; ++i) {
+		this.mElements[i].mGUIElement.Process();
 		
-		for (var i = 0; i < this.mElements.length; ++i) {
+		if (this.mOldCanvas.mX != nmain.game.mCanvasPos.mX || this.mOldCanvas.mY != nmain.game.mCanvasPos.mY) {
+			var diff = new IVec2(nmain.game.mCanvasPos.mX - this.mOldCanvas.mX,
+					nmain.game.mCanvasPos.mY - this.mOldCanvas.mY);
+			
 			var pos = new IVec2(0, 0); pos.Copy(this.mElements[i].mGUIElement.mPos);
 			this.mElements[i].mGUIElement.SetPos(pos);
 		}
-		
-		this.mOldCanvas.Copy(nmain.game.mCanvasPos);
 	}
+	
+	this.mOldCanvas.Copy(nmain.game.mCanvasPos);
 }
 
-GUIDOMContainer.prototype.Add = function(element, elementName) {
+GUIDOMContainer.prototype.AddElement = function(element, elementName) {
 	var found = false;
 	for (var i = 0; i < this.mElements.length; ++i) {
 		if (this.mElements[i].mName == elementName) {
@@ -39,16 +41,28 @@ GUIDOMContainer.prototype.Add = function(element, elementName) {
 	
 	if (found == false) {
 		var elem = new GUIDOMElement();
-		elem.mGUIElement = element;
+		
+		{
+			if (element.Type() == "GUIDOMButton") {
+				elem.mGUIElement = new GUIDOMButton();
+			}
+			else if (element.Type() == "GUIDOMInputBox") {
+				elem.mGUIElement = new GUIDOMInputBox();
+			}
+		}
+		
+		elem.mGUIElement.Copy(element);
 		elem.mName = elementName;
 		
 		this.mElements.push(elem);
 		var id = this.mElements.length - 1;
 		document.body.appendChild(this.mElements[id].mGUIElement.mElement);
+		
+		this.mElements[id].mGUIElement.RegisterCallbacks();
 	}
 }
 
-GUIDOMContainer.prototype.Remove = function(elementName) {
+GUIDOMContainer.prototype.RemoveElement = function(elementName) {
 	var id = -1;
 	for (var i = 0; i < this.mElements.length; ++i) {
 		if (this.mElements[i].mName == elementName) {
@@ -58,13 +72,27 @@ GUIDOMContainer.prototype.Remove = function(elementName) {
 	}
 	
 	if (id >= 0) {
+		this.mElements[id].mGUIElement.UnregisterCallbacks();
+		
 		document.body.removeChild(this.mElements[id].mGUIElement.mElement);
 		this.mElements.splice(id, 1);
 	}
 }
 
+GUIDOMContainer.prototype.GetElement = function(elementName) {
+	for (var i = 0; i < this.mElements.length; ++i) {
+		if (this.mElements[i].mName == elementName) {
+			return this.mElements.mGUIElement;
+		}
+	}
+	
+	throw Exception("Resource not found.");
+}
+
 GUIDOMContainer.prototype.Clear = function() {
 	for (var i = 0; i < this.mElements.length; ++i) {
+		this.mElements[i].mGUIElement.UnregisterCallbacks();
+		
 		document.body.removeChild(this.mElements[i].mGUIElement.mElement);
 	}
 	
