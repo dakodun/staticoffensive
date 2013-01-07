c980c83bfa214a3b99a8b43b7614059ccae28027
dakodun <jinto@chikin.net>
2013-01-07 14:49:17 +0000

    added method to return gui element type; peoperly copied member sin 'copy' method; added empty 'process' method for compatibility with other gui dom elements; added 'onclick' method to detect when this button was clicked; added methods to register and unregister event callbacks

--- parts/guidombutton.js
+++ parts/guidombutton.js
@@ -3,11 +3,24 @@ function GUIDOMButton() {
 	this.mPos = new IVec2(0, 0);
 	
 	this.mElement = document.createElement('button');
-	this.mText = null
+	
+	this.mHover = false;
+	this.mDown = false;
+	this.mWasClicked = false;
 };
 
+GUIDOMButton.prototype.Type = function() {
+	return "GUIDOMButton";
+}
+
 GUIDOMButton.prototype.Copy = function(other) {
+	this.mPos.Copy(other.mPos);
 	
+	this.mElement = other.mElement;
+	
+	this.mHover = other.mHover;
+	this.mDown = other.mDown;
+	this.mWasClicked = other.mWasClicked;
 }
 
 GUIDOMButton.prototype.SetUp = function(pos, text) {
@@ -17,8 +30,12 @@ GUIDOMButton.prototype.SetUp = function(pos, text) {
 	this.mElement.style.left = nmain.game.mCanvasPos.mX + this.mPos.mX + "px";
 	this.mElement.style.top = nmain.game.mCanvasPos.mY + this.mPos.mY + "px";
 	
-	this.mText = document.createTextNode(text);
-	this.mElement.appendChild(this.mText);
+	var txt = document.createTextNode(text);
+	this.mElement.appendChild(txt);
+}
+
+GUIDOMButton.prototype.Process = function() {
+	
 }
 
 GUIDOMButton.prototype.SetPos = function(pos) {
@@ -26,6 +43,59 @@ GUIDOMButton.prototype.SetPos = function(pos) {
 	
 	this.mElement.style.left = nmain.game.mCanvasPos.mX + this.mPos.mX + "px";
 	this.mElement.style.top = nmain.game.mCanvasPos.mY + this.mPos.mY + "px";
+}
+
+GUIDOMButton.prototype.OnClick = function() {
+	if (this.mWasClicked == true) {
+		this.mWasClicked = false;
+		return true;
+	}
+	
+	return false;
+}
+
+GUIDOMButton.prototype.RegisterCallbacks = function(e) {
+	this.mElement.onclick = function(e) {
+		this.mWasClicked = true;
+	}
+	
+	this.mElement.onmouseover = function(e) {
+		this.mHover = true;
+	}
+	
+	this.mElement.onmouseout = function(e) {
+		this.mHover = false;
+	}
+	
+	this.mElement.onmousedown = function(e) {
+		this.mDown = true;
+	}
+	
+	this.mElement.onmouseup = function(e) {
+		this.mDown = false;
+	}
+}
+
+GUIDOMButton.prototype.UnregisterCallbacks = function(e) {
+	this.mElement.onclick = function(e) {
+		
+	}
+	
+	this.mElement.onmouseover = function(e) {
+		
+	}
+	
+	this.mElement.onmouseout = function(e) {
+		
+	}
+	
+	this.mElement.onmousedown = function(e) {
+		
+	}
+	
+	this.mElement.onmouseup = function(e) {
+		
+	}
 }
 // ...End
 
